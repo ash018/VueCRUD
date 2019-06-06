17c26d6ddafca03ca938242698c6d52747580b84
Ash <sadatakash018@gmail.com>
2019-06-07 01:14:50 +0600

    Edit risk

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var path = require('path');
+var history = require('connect-history-api-fallback');
+
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
+const staticFileMiddleware = express.static('dist');
+
+// 1st call for unredirected requests 
+app.use(staticFileMiddleware);
+
+// Support history api 
+app.use(history({
+  index: '/dist/index.html'
+}));
+app.get('/edit/:id', function (req, res) {
+
+  // You can retrieve the :id parameter via 'req.params.id'
+  res.send('edit'+req.params.id);
+});
+// 2nd call for redirected requests
+app.use(staticFileMiddleware);
+
+
+
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
--- src/components/RiskView.vue
+++ src/components/RiskView.vue
@@ -1,5 +1,25 @@
 
+ <style>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, .5);
+  display: table;
+  transition: opacity .3s ease;
+}
 
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+ </style>
+ 
+  
 
 <template>
     <div class="container">
@@ -23,9 +43,7 @@
                     <td>{{ risk.risk_name }}</td>
                     
                     <td>
-                       
-                        <a target="_blank" class="btn btn-success" v-bind:href="'/edit/'+ risk.risk_id + '?q='+risk.risk_name" >Edit</a>
-                       
+                        <a target="_blank" class="btn btn-success" :data-node=" risk.risk_id " v-on:click="edit(risk.risk_id)" >Edit</a>
                     </td>
                     
                     <td><a target="_blank" class="btn btn-danger" v-bind:href="'/delete/'+ risk.risk_id" >Delete</a></td>
@@ -34,7 +52,49 @@
             </tbody>
         </table>
 
-        
+        <div v-if="showModal">
+            <transition name="modal">
+              <div class="modal-mask">
+                <div class="modal-wrapper">
+
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Edit Risk</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true" @click="showModal = false">&times;</span>
+                            </button>
+                        </div>
+                        <div class="container">
+                            
+                            <form v-on:submit.prevent="updateRisk">
+                              <div class="row">
+                                <div class="col-md-6">
+                                  <div class="form-group">
+                                    <label>Risk Name</label>
+                                    <input type="text" id="EditVal" class="form-control" v-model="risk.risk_name" />
+                                  </div>
+                                </div>
+                                </div>
+                                <br />
+                                <div class="form-group">
+                                  <button class="btn btn-primary" >Edit Risk</button>
+                                </div>
+                            </form>
+                          </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" @click="showModal = false">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+
+                </div>
+              </div>
+            </transition>
+          </div>
+
+  
 
     </div>
 
@@ -49,12 +109,25 @@
     export default {
         data(){
             return{
-                risks: []
+                risks: [],
+                showModal: false,
+                riskid: '',
+                risk: {
+                    risk_name: ''
+                },
             }
         },
         created: function()
         {
             this.fetchItems();
+            /*
+            let ckeditor2 = document.createElement('script');    ckeditor.setAttribute('src',"//ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js");
+            document.head.appendChild(ckeditor2);
+
+            let ckeditor = document.createElement('script');    ckeditor.setAttribute('src',"//maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js");
+            document.head.appendChild(ckeditor);
+            */
+            
         },
         methods: {
             fetchItems()
@@ -64,19 +137,27 @@
                   this.risks = response.data;
               });
             },
-            updateRisk: function() {
-              this.loading = true;
-              this.axios.put('https://risk-backend.herokuapp.com/api/risk/${this.currentRisk.risk_id}/',this.currentRisk)
-                  .then((response) => {
-                    this.loading = false;
-                    this.currentRisk = response.data;
-                    this.fetchItems();
-                  })
-                  .catch((err) => {
-                    this.loading = false;
-                    console.log(err);
-                  })
+            updateRisk(){
+                let uri = 'https://risk-backend.herokuapp.com/api/risk/'+this.riskid+'/';
+                this.axios.put(uri, this.risk).then((response) => {
+                   console.log(response);
+                   this.$router.push("/risk");
+                });
+
+                this.showModal = false;
+                window.location.reload();
             },
+            edit(id){
+                this.showModal = true;
+                let uri = 'https://risk-backend.herokuapp.com/api/risk/'+id+'/';
+                this.axios.get(uri, this.risk).then((response) => {
+                console.log(response.data.risk_name);
+                this.risk.risk_name = response.data.risk_name;
+                this.riskid = id;
+            });
+
+            }
+
         }
     }
 </script>
